7be4205d4fcc0e6abf6ada960f093fb59f82692b
saga-r-code <shah.sagar2006@gmail.com>
2024-06-26 21:30:48 +0530

    update

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -14,15 +14,15 @@ const Navbar = () => {
     <div>
       <nav>
         <ul className="flex flex-col items-center gap-6 xl:gap-10 px-4 py-5 bg-black max-w-16 h-screen rounded-e-md">
-          <li><a><FaHotel  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white"/></a></li>
-          <li><a><IoHomeOutline  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
-          <li><a><RiBillLine  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
-          <li><a><VscPieChart  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
-          <li><a><GiChefToque  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
-          <li><a><FaConciergeBell  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
-          <li><a><FaCashRegister  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
-          <li><a><IoSettingsOutline  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
-          <li><a><IoExitOutline  className="text-[#E54949] h-6 w-6  lg:h-7   hover:text-white" /></a></li>
+          <li><a><FaHotel  className="text-[#E54949] h-6 w-6  lg:h-7 hover:text-white"/></a></li>
+          <li><a><IoHomeOutline  className="text-[#E54949] h-6 w-6  lg:h-7 hover:text-white" /></a></li>
+          <li><a><RiBillLine  className="text-[#E54949] h-6 w-6  lg:h-7 hover:text-white" /></a></li>
+          <li><a><VscPieChart  className="text-[#E54949] h-6 w-6  lg:h-7 hover:text-white" /></a></li>
+          <li><a><GiChefToque  className="text-[#E54949] h-6 w-6  lg:h-7 hover:text-white" /></a></li>
+          <li><a><FaConciergeBell  className="text-[#E54949] h-6 w-6  lg:h-7  hover:text-white" /></a></li>
+          <li><a><FaCashRegister  className="text-[#E54949] h-6 w-6  lg:h-7  hover:text-white" /></a></li>
+          <li><a><IoSettingsOutline  className="text-[#E54949] h-6 w-6  lg:h-7  hover:text-white" /></a></li>
+          <li><a><IoExitOutline  className="text-[#E54949] h-6 w-6  lg:h-7  hover:text-white" /></a></li>
         </ul>
       </nav>
     </div>
